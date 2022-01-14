fd3105df367b8e7df91760ad842a06840915c113
Emil Indzhev <emil.indjev@gmail.com>
2022-01-14 08:22:06 +0200

    removed garbage files

--- hint/testing/hint.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-
-std::vector<bool> genHint(const std::vector<int>& a, const std::vector<int>& b, const std::vector<int>& sol);
-std::vector<int> solve(const std::vector<int>& a, const std::vector<int>& b, const std::vector<bool>& hint);

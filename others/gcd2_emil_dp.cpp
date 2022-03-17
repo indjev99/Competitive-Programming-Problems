f23ccaabcb06591385c56230a3bffcf22d3e51fe
Emil Indzhev <emil.indjev@gmail.com>
2022-03-17 20:49:45 +0200

    Add my solutions to gcd2

---
+++ others/gcd2_emil_dp.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+
+const int MAX_N = 2e5;
+const int MAX_Q = 2e5;
+const int MAX_VAL = 2e6;
+const long long MOD = 1e9 + 7;
+
+int n, q;
+
+int smallestPrime[MAX_VAL + 1];
+
+long long dp[MAX_VAL + 1];
+
+long long cnt[MAX_VAL + 1];
+
+long long numSubsets[MAX_N + MAX_Q];
+
+long long ans;
+
+void precompPows2()
+{
+    numSubsets[0] = 1;
+
+    for (int i = 1; i <= MAX_N + MAX_Q; ++i)
+    {
+        numSubsets[i] = numSubsets[i - 1] * 2 % MOD;
+    }
+
+    for (int i = 0; i <= MAX_N + MAX_Q; ++i)
+    {
+        numSubsets[i] = (numSubsets[i] + MOD - 1) % MOD;
+    }
+}
+
+void sieve()
+{
+    for (int i = 2; i <= MAX_VAL; ++i)
+    {
+        if (smallestPrime[i] > 0) continue;
+        for (int j = i; j <= MAX_VAL; j += i)
+        {
+            if (smallestPrime[j] == 0) smallestPrime[j] = i;
+        }
+    }
+}
+
+void addNumber(int num, int sign)
+{
+    std::vector<int> divisors = {1};
+    int oldDivisors = 0;
+    int prevPrime = 0;
+    int currPrimePow = 0;
+
+    while (num > 1)
+    {
+        int prime = smallestPrime[num];
+        if (prime != prevPrime)
+        {
+            prevPrime = prime;
+            currPrimePow = prime;
+            oldDivisors = divisors.size();
+        }
+        else currPrimePow *= prime;
+
+        num /= prime;
+
+        for (int i = 0; i < oldDivisors; ++i)
+        {
+            divisors.push_back(divisors[i] * currPrimePow);
+        }
+    }
+
+    for (int div : divisors)
+    {
+        cnt[div] += sign;
+    }
+}
+
+void findAns()
+{
+    for (int i = MAX_VAL; i >= 1; --i)
+    {
+        dp[i] = (long long) numSubsets[cnt[2]];
+        for (int j = i * 2; j <= MAX_VAL; j += i)
+        {
+            dp[i] -= dp[j];
+        }
+    }
+
+    ans = 0;
+
+    for (int i = 1; i <= MAX_VAL; ++i)
+    {
+        ans += i * dp[i];
+        ans %= MOD;
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int type, num;
+
+    std::cin >> n >> q;
+
+    precompPows2();
+    sieve();
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> num;
+        addNumber(num, 1);
+    }
+
+    findAns();
+    std::cout << ans << "\n";
+
+    for (int i = 0; i < q; ++i)
+    {
+        std::cin >> type >> num;
+        addNumber(num, type == 1 ? 1 : -1);
+
+        findAns();
+        std::cout << ans << "\n";
+    }
+
+    return 0;
+}
